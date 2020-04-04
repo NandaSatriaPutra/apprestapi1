1bd5275f6879a93f6772a71c7be32658accfedb9
NandaSatriaPutra <nsatria993@gmail.com>
2020-04-05 01:42:36 +0700

    menampilkan data mahasiswa

--- routes.js
+++ routes.js
@@ -1,7 +1,12 @@
 'use strict';
-module.exports= function(app){
-    var jsonku = require ('./controller');
 
-    app.route('./')
+module.exports = function(app) {
+    var jsonku = require('./controller');
+
+    app.route('/')
         .get(jsonku.index);
+    app.route('/tampil')
+        .get(jsonku.tampilsemuamahasiswa);
+
+
 }
